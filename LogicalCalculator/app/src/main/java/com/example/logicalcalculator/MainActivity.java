41d43bdc4f87da3524606a54a61f2cb606c6a592
kuldeepjhala-dev <kuldeephjhala@gmail.com>
2024-01-13 22:45:53 +0530

    Implemented drawables in logicalCalculator

--- LogicalCalculator/app/src/main/java/com/example/logicalcalculator/MainActivity.java
+++ LogicalCalculator/app/src/main/java/com/example/logicalcalculator/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.logicalcalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,49 +9,69 @@ import android.widget.EditText;
 public class MainActivity extends Activity {
 
     EditText etDisplay;
-    Button btnAc, btnBack, btnPer, btnDiv, btnMul, btnSub, btnAdd, btnEquals, btnDot, btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    Button b[] = new Button[19];
+    int id[] = {R.id.btAc, R.id.btBack, R.id.btPer, R.id.btDiv,
+            R.id.btn7, R.id.btn8, R.id.btn9, R.id.btnMul,
+            R.id.btn4, R.id.btn5, R.id.btn6, R.id.btnSub,
+            R.id.btn1, R.id.btn2, R.id.btn3, R.id.btnAdd,
+            R.id.btn0, R.id.btnDot, R.id.btnEquals};
+
+    int i, flag = 0;
+    String num = "", number = "";
+    String range = "0123456789";
+
+    String operation = "", value1 = "", value2 = "" , addTemp = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         etDisplay = findViewById(R.id.et);
-        btnAc = findViewById(R.id.btAc);
-        btnBack = findViewById(R.id.btBack);
-        btnPer = findViewById(R.id.btPer);
-        btnDiv = findViewById(R.id.btDiv);
-        btnMul = findViewById(R.id.btnMul);
-        btnSub = findViewById(R.id.btnSub);
-        btnAdd = findViewById(R.id.btnAdd);
-        btnEquals = findViewById(R.id.btnEquals);
-        btnDot = findViewById(R.id.btnDot);
-        btn0 = findViewById(R.id.btn0);
-        btn1 = findViewById(R.id.btn1);
-        btn2 = findViewById(R.id.btn2);
-        btn3 = findViewById(R.id.btn3);
-        btn4 = findViewById(R.id.btn4);
-        btn5 = findViewById(R.id.btn5);
-        btn6 = findViewById(R.id.btn6);
-        btn7 = findViewById(R.id.btn7);
-        btn8 = findViewById(R.id.btn8);
-        btn9 = findViewById(R.id.btn9);
 
-        btnAc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        for (i = 0; i < b.length; i++) {
+            b[i] = findViewById(id[i]);
+            b[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
-                etDisplay.setText("0");
+                    Button bt = (Button) v;
+                    num = bt.getText().toString();
 
-            }
-        });
+                    if (range.contains(num)) {
+                        number = number + num;
+                        etDisplay.setText(number);
+                    } else if (num.equals("AC")) {
+                        etDisplay.setText("0");
+                        number = "";
+                    } else if (num.equals("back")) {
+                        if (number.length() > 0) {
+                            number = number.substring(0, number.length() - 1);
+                            etDisplay.setText(number);
+                        } else {
+                            number = "";
+                            etDisplay.setText("0");
+                        }
+                    }
 
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                    if(number.length()>1){
+                        if(num.equals("+")  && flag==0){
+                            value1 = number;
+                            operation = num;
+                            flag = 1;
+                            number = number + operation;
+                            etDisplay.setText(number);
+                        }
+//                        else if(operation.equals("+") && flag==1){
+//
+//                        }
 
-            }
-        });
+                    }
 
+                }
+            });
+
+        }
 
 
     }
